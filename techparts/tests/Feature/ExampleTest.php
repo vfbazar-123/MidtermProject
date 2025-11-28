2ec2091f115b9b096346221fa04c69865fb4b140
EcommerceGroup <kennethlostzzvil00@gmail.com>
2025-11-28 14:20:05 +0800

    Fix: Enable RefreshDatabase in feature test to handle migrations

--- techparts/tests/Feature/ExampleTest.php
+++ techparts/tests/Feature/ExampleTest.php
@@ -2,17 +2,18 @@
 
 namespace Tests\Feature;
 
-// use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
 class ExampleTest extends TestCase
 {
+    use RefreshDatabase;
     /**
      * A basic test example.
      */
     public function test_the_application_returns_a_successful_response(): void
     {
-        $response = $this->get('/');
+        $response = $this->followingRedirects()->get('/');
 
         $response->assertStatus(200);
     }
